e258b8403cfb12ec72f9b5fa64a974ba5dea5017
malimin <zhrmghgfff@163.com>
2020-05-13 17:47:14 +0800

    创建工程

---
+++ MLib/MLib.h
@@ -0,0 +1,19 @@
+//
+//  MLib.h
+//  MLib
+//
+//  Created by zhrmghgfff on 2020/5/13.
+//  Copyright © 2020 zhrmghgfff. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MLib.
+FOUNDATION_EXPORT double MLibVersionNumber;
+
+//! Project version string for MLib.
+FOUNDATION_EXPORT const unsigned char MLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MLib/PublicHeader.h>
+
+
